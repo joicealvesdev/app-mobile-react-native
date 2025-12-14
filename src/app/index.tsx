2de7f451c5b316947748aa1216ed0481f685b558
Joice Alves <joicealvesrt18@gmail.com>
2025-12-13 21:32:47 -0300

    modificação no layout.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,27 @@
-import { View, Text, TouchableOpacity, FlatList,Image } from "react-native";
-import { useRouter } from "expo-router";
+import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
+import { router } from "expo-router";
 import { useState } from "react";
 
+type Link = {
+  id: string;
+  url: string;
+};
+
 export default function Index() {
-  const router = useRouter();
-  const [links, setLinks] = useState([
+  const [links, setLinks] = useState<Link[]>([
     { id: "1", url: "https://rocketseat.com.br" },
     { id: "2", url: "https://expo.dev" },
   ]);
 
   return (
     <View style={{ flex: 1, padding: 20 }}>
-    
-       <Image
-  source={require("../../assets/images/splash.png")}
-  style={{ width: 120, height: 120, alignSelf: "center", marginBottom: 20 }}
-  resizeMode="contain"
-/>
+      <Image
+        source={require("../../assets/images/splash.png")}
+        style={{ width: 120, height: 120, alignSelf: "center", marginBottom: 20 }}
+        resizeMode="contain"
+      />
 
-      <FlatList
+      <FlatList<Link>
         data={links}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
@@ -32,6 +35,7 @@ export default function Index() {
           backgroundColor: "#7c3aed",
           padding: 15,
           borderRadius: 8,
+          marginTop: 20,
         }}
       >
         <Text style={{ color: "#fff", textAlign: "center" }}>
