48ee417344d2abd204a1fe1903cc021ec7cf9406
Joice Alves <joicealvesrt18@gmail.com>
2025-12-15 14:38:43 -0300

    new

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -5,6 +5,7 @@ import { colors } from "../../styles/colors";
 import { router } from "expo-router";
 import { Categories } from "../../components/category/categories";
 import { Input } from "../../components/input";
+import { Button } from "../../components/button";
 export default function Add() {
   return (
     <View style={styles.container}>
@@ -21,6 +22,7 @@ export default function Add() {
         <View style={styles.form}>
         <Input placeholder="Nome" />
         <Input placeholder="Url" />
+         <Button title="Adicionar" />
       </View>
     </View>
   )
